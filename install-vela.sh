0b1984f38ca4307894e0bad69039ec67e79cea9a
barnettZQG <barnett.zqg@gmail.com>
2022-08-03 11:58:55 +0800

    Fix: change the default version to beta.6
    Signed-off-by: barnettZQG <barnett.zqg@gmail.com>

--- install-vela.sh
+++ install-vela.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-VERSION=${VERSION:v1.5.0-beta.6}
+VERSION=${VERSION:-"v1.5.0-beta.6"}
+
+if [ "$VERSION" == "latest" ]
+then
+  VERSION="v1.5.0-beta.6"
+fi
 
 PLATFORM=linux-amd64
 
@@ -8,13 +13,15 @@ if [ "$BUILDPLATFORM" == "linux/arm64" ]
 then
     PLATFORM=linux-arm64
 fi
+echo "Download the binary from: https://github.com/kubevela/kubevela/releases/download/$VERSION/vela-$VERSION-$PLATFORM.tar.gz"
 
 curl -LO https://github.com/kubevela/kubevela/releases/download/$VERSION/vela-$VERSION-$PLATFORM.tar.gz
 if [ ! -d "vela" ]; then
   mkdir -p vela
 fi
-tar -zxf vela-$VERSION-$PLATFORM.tar.gz -C vela
-mv vela/linux-amd64/vela /usr/local/bin/vela
+
+tar -zxf vela-$VERSION-$PLATFORM.tar.gz -C vela || exit 1
+mv vela/linux-amd64/vela /usr/local/bin/vela || exit 1
 rm vela-$VERSION-$PLATFORM.tar.gz 
 rm -r vela
 vela version
